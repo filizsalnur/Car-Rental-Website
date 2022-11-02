26edbaaf20c650bc88571b96848c5bcb50598aae
filizsalnur <92436947+filizsalnur@users.noreply.github.com>
2022-11-02 20:28:36 +0300

    Agil kurma

--- src/main.js
+++ src/main.js
@@ -3,14 +3,17 @@ import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import LitepieDatepicker from 'litepie-datepicker';
+import VueAgile from 'vue-agile'
 
 
 const app = createApp(App)
 
-// router setup
 import router from "./routers/routers";
+
+Vue.use(VueAgile)
 app.use(router)
 app.use(LitepieDatepicker);
+
 app.mount('#app');
 
 import "bootstrap/dist/js/bootstrap.js"
