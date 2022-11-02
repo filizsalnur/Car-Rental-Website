7754bc4f57c1502302be43fab43f1cf0ea5bda92
filizsalnur <92436947+filizsalnur@users.noreply.github.com>
2022-11-02 20:02:45 +0300

    ana sayfa kodları

---
+++ src/components/AnaSayfa.vue
@@ -0,0 +1,310 @@
+<template>
+  <div clas="background"><div class="center">
+    <div class="text1">SENİN İÇİN EN UYGUN ARACI</div>
+    <div class="text2">ara, bul ve kirala</div>
+    <div class="text3">En uygun fiyat garantisi ile ister online öde, ister aracı teslim alırken öde!</div>
+    
+    <div class="container">
+      <div class="row">
+      <div class="col">
+        <div class="nav-left">
+        <button class="button1">Günlük Kiralama</button>
+        <button class="button2">Aylık Kiralama</button>
+      </div>
+      </div>
+      <div class="col">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <img src="@/images/AnaSayfa/Icon.png" alt="icon" class="icon"> 
+      </div>
+      <div class="col">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="nav-left">
+        <button class="button3" @click="aramaBariController=!aramaBariController"><div class="text4"> <img src="@/images/AnaSayfa/BlueIcon.png" alt="icon"> Nereden Alacaksınız?</div></button>
+        </div>
+      </div>
+      <div class="col">
+        <div class="nav-left">
+        <button class="button4"  @click="tarihController=!tarihController"><div class="text4"><img src="@/images/AnaSayfa/tarihIcon.png" alt="icon"> Alış Tarihi  |
+          <img src="@/images/AnaSayfa/tarihIcon.png" alt="icon"> Bırakış Tarihi</div></button>
+          <button class="button5">Uygun Araçları Bul <img src="@/images/AnaSayfa/searchIcon.png" alt="icon"></button>
+        </div>
+        <div v-if="tarihController==true">
+          <div class="flex">
+              <litepie-datepicker
+                placeholder="My Placeholder"
+                v-model="dateValue"
+              ></litepie-datepicker>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="nav-left">
+        <button class="button3" v-if="checker==true"><div class="text4"> <img src="@/images/AnaSayfa/RedIcon.png" alt="icon"> Nerede Bırakacaksınız?</div></button>
+        </div>
+      </div>
+      <div class="col"></div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="text6">
+          <input type="checkbox" id="checkbox" v-model="checked" @click="checker=!checker" />
+          <label for="checkbox" style="margin-left:5px"> Farklı bir yerde teslim etmek istiyorum</label>
+        </div>
+      </div>
+      <div class="col"></div>
+    </div>    
+  </div>
+  <div class="icon2"><img src="@/images/AnaSayfa/plus.png" alt="icon" ></div>
+  <div class="space1">
+  <div class="container">
+      <div class="row">
+      <div class="col">
+        <div class="text8">ÖN ÖDEME FIRSATI İLE</div>
+        <div class="text9">araç kiralamak artık çok kolay!</div>
+      </div></div>
+  </div>
+  
+</div>
+</div>
+</div> 
+<div class="center">
+  <div class="image2"></div>
+  <div class="text7">Tüm Kampanyaları Gör <img src="@/images/AnaSayfa/KampanyaIcon.png" alt="icon" ></div>
+</div> 
+  <FooterApp/>
+ 
+</template>
+
+<script >
+import { ref } from 'vue';
+import LitepieDatepicker from 'litepie-datepicker';
+import FooterApp from './FooterApp.vue';
+export default {
+  name: "AnaSayfa",
+  components: {
+    FooterApp,
+    LitepieDatepicker,
+  },
+  data() {
+    return {
+      aramaBariController:false,
+      checker:false,
+      tarihController:false,
+    }
+  },
+  setup() {
+      const dateValue = ref([]);
+      
+      return {
+        dateValue
+      };
+    },
+  methods: {
+  
+  },
+}
+</script>
+
+<style scoped>
+.background{
+  background-image: url("@/images/AnaSayfa/background.png");
+}
+.center{
+  margin:0 auto;
+  width: 1220px;
+}
+.center2{
+  margin:0 auto;
+  width: 1226px;
+}
+.nav-left{
+  margin-left: 0;
+  left:0 ;
+  position: relative; 
+  float: left;
+  margin-bottom: 0;
+}
+.text1{
+  position: relative;
+  font-family: "Quicksand" ;
+  font-size: 18pt;
+  color: #ffffff;
+  text-align: left;
+  padding-top: 100px;
+}
+.text2{
+  position: relative;
+  font-family: "Quicksand" ;
+  font-size: 60pt;
+  color: #ffffff;
+  text-align: left;
+  line-height: 60pt;
+}
+.text3{
+  position: relative;
+  font-family: "PoppinsBold" ;
+  font-size: 13pt;
+  color: #ffffff;
+  text-align: left;
+  padding-top: 35px;
+}
+.text4{
+  padding-left: 28px;
+  text-align: left;
+  position: relative; 
+}
+.text5{
+  padding-left: 28px;
+}
+.text6{
+  margin-top:20px;
+  padding-left: 10px;
+  color: #ffffff;
+  position: relative; 
+  float: left;
+}
+.text7{
+  position: relative;
+  font-family: "Quicksand" ;
+  font-size: 13pt;
+  color: #2a2a2a;
+  margin-left: 6px;
+  padding-top: 5px;
+}
+.text8{
+  position: relative;
+  font-family: "Quicksand" ;
+  font-size: 20pt;
+  color: #ffffff;
+  text-align: left;
+  margin-left: 75px;
+  margin-top:70px;
+}
+.text9{
+  position: relative;
+  font-family: "Quicksand" ;
+  font-size: 15pt;
+  color: #ffffff;
+  text-align: left;
+  margin-left: 75px;
+  margin-top:0px;
+}
+.button1 {
+  height: 40px;
+  width : 162px;
+  background-color: #ffffff;
+  color: #2a2a2a;
+  border: none;
+  font-family: "Quicksand" ;
+  font-size: 11pt;
+  margin-right: 3px;
+  margin-top: 18px;
+  border-radius: 8px;
+}
+.button2 {
+  height: 40px;
+  width : 162px;
+  background-color: #25459a;
+  color: #ffffff;
+  border: none;
+  font-family: "Quicksand" ;
+  font-size: 11pt;
+  margin-top: 18px;
+  border-radius: 8px;
+}
+.button3 {
+  height: 59px;
+  width : 594px;
+  background-color: #ffffff;
+  color: #2a2a2a;
+  border: none;
+  font-family: "Quicksand" ;
+  font-size: 14pt;
+  margin-top: 6px;
+  border-radius: 8px;
+}
+.button4 {
+  height: 59px;
+  width : 349px;
+  background-color: #ffffff;
+  color: #2a2a2a;
+  border: none;
+  font-family: "Quicksand" ;
+  font-size: 12pt;
+  margin-top: 6px;
+  border-radius: 8px;
+}
+.button5 {
+  height: 59px;
+  width : 197px;
+  background-color: #e51c3d;
+  color: #ffffff;
+  border: none;
+  font-family: "Quicksand" ;
+  font-size: 12pt;
+  margin-top: 6px;
+  margin-left:15px;
+  border-radius: 8px;
+}
+.space1 {
+  height: 306px;
+  width : 1220px;
+  background-color: #25459a;
+  color: #ffffff;
+  border: none;
+  font-family: "Quicksand" ;
+  font-size: 12pt;
+  margin-top: 30px;
+  border-radius: 8px;
+  background-repeat: no-repeat;
+  background-size:cover;
+  z-index: 15;
+  float:left;
+}
+.image{
+  background: url("@/images/AnaSayfa/background.png") ;
+  background-repeat: no-repeat;
+  background-size:cover;
+  height: 739px;
+  width: 1920px
+  
+}
+.image2 {
+  color: #ffffff;
+  border: none;
+  font-family: "Quicksand" ;
+  font-size: 12pt;
+  margin-top: 160px;
+  margin-left: 6px;
+  border-radius: 8px;
+  background: url("@/images/AnaSayfa/background2.png") ;
+  background-repeat: no-repeat;
+  background-size:cover;
+  height: 240px;
+  width: 1220px;
+
+}
+.icon{
+  margin-left: 72px;
+  position: relative; 
+  float: left;
+  margin-top: 0;
+  top: 0;
+  line-height: 0;
+}
+.icon2{
+	position: absolute;
+	z-index: 15;
+	left: 738px;
+	top: 589px;
+}
+
+</style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="navbar">
+    <div class="center">
+    
+      <img alt="Logo" src="@/images/Header/Logo.png" class="logo">
+
+        <div style="float:right">
+        <div style="overflow:hidden">
+        <div class="nav-right">
+        <button class="button1" v-if="logController==false">Giriş Yap | Üye Ol</button>
+        <button class="button1" v-if="logController==true" @click="clickController=!clickController"><img alt="Turkey" src="@/images/Header/LogIcon.png" style="margin-top:0px"> Enver Yıldırım
+        </button>
+        <button class="button2">Rezervasyon Sorgula</button>
+        <button class="button3">Türkçe <img alt="Turkey" src="@/images/Header/Turkey.png"> TRY <img alt="icon" src="@/images/Header/IconOk.png"></button>
+            <img  src="@/images/Header/Icon.png" alt="icon" class="icon">
+          <div style="float:right">
+            <div class="text1">7/24 Müşteri Destek</div>
+            <div class="text2">0850 888 1 864</div>
+          </div>
+      </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default ({
+  name:'HeaderApp',
+  data() {
+    return {
+      logController:false,
+      clickController:false,
+    }
+  },
+  methods: {
+    
+  }
+})
+</script>
+
+<style scoped>
+.navbar{
+  height: 80px;
+  background: #ffffff;
+}
+.nav-right{
+  padding-top: 12px;
+  display: block;
+  
+
+}
+.center{
+
+  margin:0 auto;
+  width: 1220px;
+}
+.logo{
+  position: relative;
+  width: 169px;
+  height: 23px;
+  left: 0px;
+  float:left;
+  margin-top: 16px;
+}
+.icon{
+  position: relative;
+  width: 36px;
+  height: 36px;
+  margin-right: 10px;
+}
+
+
+.text1{
+  font-family: "Quicksand" ;
+  font-size: 11pt;
+  color: #25459a;
+  margin-bottom: 0;
+  padding-bottom: 0;
+  line-height: 17pt;
+}
+.text2{
+  font-family: "Poppins" ;
+  font-size: 13pt;
+  color: #373737;
+  margin-top: 0;
+  padding-top: 0;
+  line-height: 10pt;
+}
+.button1 {
+  height: 37px;
+  width : 153px;
+  background-color: #25459a;
+  color: #ffffff;
+  border: 1px solid #25459a;
+  font-family: "Quicksand" ;
+  font-size: 11pt;
+  margin-right: 8px;
+  border-radius: 8px;
+}
+.button2 {
+  height: 37px;
+  width : 181px;
+  background-color: #ffffff;
+  color: #25459a;
+  border: 1px solid #98a7cf;
+  font-family: "Quicksand" ;
+  font-size: 11pt;
+  margin-right: 8px;
+  cursor: pointer;
+  border-radius: 8px;
+}
+.button3 {
+  height: 37px;
+  width : 164px;
+  background-color: #ffffff;
+  color: #2a2a2a;
+  border: 1px solid #25459a;
+  font-family: "Quicksand" ;
+  font-size: 11pt;
+  margin-right: 30px;
+  border-radius: 8px;
+}
+.button1:hover {
+  border: 3px solid #98a7cf;
+}
+.button2:hover {
+  border: 3px solid #98a7cf;
+}
+.button3:hover {
+  border: 3px solid #25459a;
+}
+</style>
